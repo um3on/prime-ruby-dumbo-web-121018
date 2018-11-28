80e0b0237de58d1cc0995aaa143c028ffb25cb08
M. T <mariam.tamimi@gmail.com>
2018-11-28 01:12:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  for x in 2..Math.sqrt(num) do
+    if num % x == 0
+      return false
+    end
+  end
+  return true
+end
