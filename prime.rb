4dd23b5a685c538a1c225417e913510cce91f147
M. T <mariam.tamimi@gmail.com>
2018-11-28 01:11:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
 # Add  code here!
 def prime?(num)
+  if num < 1
+    return false
+  end
+  if num == 1
+    return true
+  end
+
   for x in 2..Math.sqrt(num) do
     if num % x == 0
       return false
